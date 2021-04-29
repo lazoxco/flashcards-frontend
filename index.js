b3d5c04abc69dca3a3c209d00701abb5b4b2bb6c
github username <github email address>
2021-04-29 18:25:25 -0400

    Removed old code.

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-const baseURL = "http://localhost:3000"
+const baseURL = 'http://localhost:3000'
 const flashcardsURL = baseURL + '/flashcards'
 
 const cardsDiv = document.getElementById('card-list')
-
-
-fetch("http://localhost:3000/flashcards")
-.then(resp => resp.json())
-.then(data => showCards(data))
-
-// function showCards(cardData){
-//   const cards = cardData
-
-  // cards.forEach(card => {
-  //   const div = document.createElement('div')
-  //   const cardDiv = document.createElement('div')
-  //   const cardText = document.createElement('h2')
-
-  //   div.classList.add('col-lg-4', 'col-md-6')
-  //   cardDiv.classList.add('card')
-  //   cardText.innerText = `${card.question}`
-  //   cardDiv.appendChild(cardText)
-  //   cardsDiv.appendChild(cardDiv)
-  // })
-  
-// }
