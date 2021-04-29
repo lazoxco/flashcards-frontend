f53a8407d5504768366cc7a5648a1f83290544c8
github username <github email address>
2021-04-28 22:06:40 -0400

    Created function to display cards from API.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+
+const cardsDiv = document.getElementById('card-list')
+
+
 fetch("http://localhost:3000/flashcards")
 .then(resp => resp.json())
 .then(data => showCards(data))
 
 function showCards(cardData){
   const cards = cardData
-  debugger
+  cards.forEach(card => {
+    const div = document.createElement('div')
+    const cardDiv = document.createElement('div')
+    const cardText = document.createElement('h2')
+
+    div.classList.add('col-lg-4', 'col-md-6')
+    cardDiv.classList.add('card')
+    cardText.innerText = `${card.question}`
+    cardDiv.appendChild(cardText)
+    div.appendChild(cardDiv)
+
+    cardsDiv.appendChild(div)
+    debugger
+  })
 }
