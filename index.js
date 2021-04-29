4608abd4a94f0505f5b6cf6f482c12882b2cc478
github username <github email address>
2021-04-29 17:49:13 -0400

    Building out speparate files for flashcard class.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const baseURL = "http://localhost:3000"
+const flashcardsURL = baseURL + '/flashcards'
 
 const cardsDiv = document.getElementById('card-list')
 
@@ -6,19 +8,19 @@ fetch("http://localhost:3000/flashcards")
 .then(resp => resp.json())
 .then(data => showCards(data))
 
-function showCards(cardData){
-  const cards = cardData
-  cards.forEach(card => {
-    const div = document.createElement('div')
-    const cardDiv = document.createElement('div')
-    const cardText = document.createElement('h2')
+// function showCards(cardData){
+//   const cards = cardData
 
-    div.classList.add('col-lg-4', 'col-md-6')
-    cardDiv.classList.add('card')
-    cardText.innerText = `${card.question}`
-    cardDiv.appendChild(cardText)
-    div.appendChild(cardDiv)
+  // cards.forEach(card => {
+  //   const div = document.createElement('div')
+  //   const cardDiv = document.createElement('div')
+  //   const cardText = document.createElement('h2')
 
-    cardsDiv.appendChild(div)
-  })
-}
+  //   div.classList.add('col-lg-4', 'col-md-6')
+  //   cardDiv.classList.add('card')
+  //   cardText.innerText = `${card.question}`
+  //   cardDiv.appendChild(cardText)
+  //   cardsDiv.appendChild(cardDiv)
+  // })
+  
+// }
--- src/flashcard.js
+++ src/flashcard.js
@@ -1,8 +1,32 @@
 class Flashcard {
-  constructor(id, question, answer, topic_id){
+
+  static all = []
+  constructor({id, question, answer, topic_id}){
     this.id = id
     this.question = question
     this.answer = answer
     this.topic_id = topic_id
+
+    this.element = document.createElement('div')
+    this.element.id = `flashcard-${id}`
+    this.element.dataset.id = id
+
+    Flashcard.all.push(this)
   }
+
+  createCard(){
+    this.element.innerHTML = `
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <h2>${this.question}</h2>
+      </div>
+    </div>
+    `
+    return this.element
+  }
+
+  attachToDom(){
+    cardsDiv.appendChild(this.createCard())
+  }
+  debugger
 }
